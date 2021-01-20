af76fa2fed379d2f460112a989ebc03d2a17abc2
Etienne Pelletier <etienne.pelletier@canada.ca>
2021-01-20 19:42:38 +0000

    initial aqhi realtime loader

--- msc_pygeoapi/loader/__init__.py
+++ msc_pygeoapi/loader/__init__.py
@@ -46,6 +46,7 @@ try:
     from msc_pygeoapi.loader.swob_realtime import swob_realtime
     from msc_pygeoapi.loader.ltce import ltce
     from msc_pygeoapi.loader.climate_archive import climate_archive
+    from msc_pygeoapi.loader.aqhi_realtime import aqhi_realtime
 except ImportError as err:
     LOGGER.info('loaders not imported')
     LOGGER.debug(err)
@@ -70,6 +71,7 @@ try:
     data.add_command(swob_realtime)
     data.add_command(ltce)
     data.add_command(climate_archive)
+    data.add_command(aqhi_realtime)
 except NameError as err:
     LOGGER.info('loaders not found')
     LOGGER.debug(err)
